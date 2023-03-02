e4d9f084400c87896a651916bb9bb0bb096ec0bb
hanbonghun <bng4535@khu.ac.kr>
2023-03-02 09:12:15 +0900

    1단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
-
+import java.io.*;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("명언 앱");
+        while(true){
+            System.out.print("명령 ) ");
+            String cmd = sc.nextLine();
+            if(cmd.equals("종료")){
+                return;
+            }
+        }
     }
 }
