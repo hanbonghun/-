c8f64732b4fdcfb2caf80439cae0e6137f383bfc
hanbonghun <bng4535@khu.ac.kr>
2023-03-03 04:45:36 +0900

    리팩토링 : 명언 등록, 목록 기능 구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,30 @@ import java.util.*;
 
 public class App {
     public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        SystemController systemController = new SystemController();
+        WiseSayingController wiseSayingController = new WiseSayingController();
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = Container.getScanner().nextLine().trim();
+            Rq  rq = new Rq(command);
+
+            switch (rq.getActionCode()) {
+                case "종료":
+                    systemController.exit();
+                    break;
+                case "등록":
+                    wiseSayingController.write();
+                    break;
+                case "목록":
+                    wiseSayingController.list();
+                    break;
+
+
+            }
+        }
 
     }
 
