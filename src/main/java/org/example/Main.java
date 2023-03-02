a0eb19bc692ebccd66fd5d30005b926197b828e2
hanbonghun <bng4535@khu.ac.kr>
2023-03-02 09:14:25 +0900

    2단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("명언 앱");
+        System.out.println("== 명언 앱 ==");
         while(true){
             System.out.print("명령 ) ");
             String cmd = sc.nextLine();
             if(cmd.equals("종료")){
                 return;
+            }else if (cmd.equals("등록")){
+                System.out.print("명언 : ");
+                String quote = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
             }
         }
     }
