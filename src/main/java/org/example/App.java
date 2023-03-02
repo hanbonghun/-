a62af8a20c1b5569ea427d84316eaf76b8783001
hanbonghun <bng4535@khu.ac.kr>
2023-03-03 04:41:36 +0900

    리팩토링 : App.java 추가 main 에서 바로 실행되지 않게끔

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.system.controller.SystemController;
+import org.example.wiseSaying.controller.WiseSayingController;
+import org.example.wiseSaying.entity.WiseSaying;
+
+import java.util.*;
+
+public class App {
+    public void run() {
+
+    }
+
+}
